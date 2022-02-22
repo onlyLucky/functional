70b857efa3b0810ebafdaa293f3f4d9b3050cb4d
feynman <2901954062@qq.com>
2022-02-22 11:03:45 +0800

    新增ramda、lodash库文档

--- src/compose.js
+++ src/compose.js
@@ -2,9 +2,11 @@
  * @Author: pink
  * @Date: 2022-02-22 10:13:37
  * @LastEditors: pink
- * @LastEditTime: 2022-02-22 10:36:44
+ * @LastEditTime: 2022-02-22 10:54:11
  * @Description: 代码组合
  */
+
+const { replace } = require("lodash")
 
 let compose = function (f,g) {
   return function(x){
@@ -34,3 +36,13 @@ last(['jumpkick', 'roundhouse', 'uppercut']);//=> 'uppercut'
 
 // 结合律（associativity）
 let associative = compose(f, compose(g, h)) == compose(compose(f, g), h);
+
+//非 pointfree，因为提到了数据：word
+
+let snakeCase = function(word){
+  return word.toLowerCase().replace(/\s+/ig, '_')
+}
+let toLowerCase = function(x) { return x.toLowerCase(); };
+let snakeCasePoinfree = compose(replace(/\s+/ig, '_'),toLowerCase)
+
+console.log(snakeCasePoinfree('Hybird ONE'))
